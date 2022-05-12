5d7ce90ef6baaba50718b695663c2f5a765a0a3e
REPUBLIC OF GAMERS <shaxzodpugb1@gmail.com>
2022-05-12 14:10:09 +0500

    mainga.xml ga button qo'shdim va uni bosganda SecondaryActivityga kiradigan qildim!

--- app/src/main/java/com/example/kotlinaplicationtestgithub/MainActivity.kt
+++ app/src/main/java/com/example/kotlinaplicationtestgithub/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.kotlinaplicationtestgithub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        open_layer()
     }
+
+    fun open_layer(){
+        var b_layer_open=findViewById<Button>(R.id.b_layer_open)
+        b_layer_open.setOnClickListener {
+            activityLayer()
+        }
+    }
+
+    fun activityLayer(){
+        val intent=Intent(this,SecondActivity::class.java)
+        startActivity(intent)
+    }
+
 }
